1bbc11def80cafc018e09ee798b56cff0df08679
davidkelley <david.james.kelley@gmail.com>
2018-08-09 15:12:04 +0100

    ci: fixing broken build cache

--- plugins/plugin-pipeline/src/lib/poll.js
+++
@@ -1,9 +0,0 @@
-const { Observable } = require('rxjs');
-
-module.exports = (resolver, { interval = 500 } = {}) =>
-  new Observable(observer => {
-    const id = setInterval(async () => {
-      observer.next(await resolver());
-    }, interval);
-    return () => clearInterval(id);
-  });
