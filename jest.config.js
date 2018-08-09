eed9f73e0c9d808d2b3ce9f57c4e8948612c949c
davidkelley <david.james.kelley@gmail.com>
2018-08-09 14:59:00 +0100

    ci: exporting test metadata

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  reporters: ['default', 'jest-junit'],
   modulePathIgnorePatterns: ['/__fixtures__/'],
   roots: ['<rootDir>/core', '<rootDir>/plugins'],
   testEnvironment: 'node',
